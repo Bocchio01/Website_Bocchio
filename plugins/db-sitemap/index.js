5d5af084e17e6ac53b0699121fa8f40a70c52307
Bocchio01 <tommaso.bocchietti@gmail.com>
2022-02-26 05:19:53 +0100

    Fixing curl query

--- plugins/db-sitemap/index.js
+++ plugins/db-sitemap/index.js
@@ -2,10 +2,10 @@ module.exports = {
   onPostBuild: async ({ constants, utils }) => {
     // onPreBuild: async ({ utils }) => {
     console.log('Building complete -> Updating DB with new sitemap')
-    console.log(`curl -X POST -d "api_key=${process.env.API_KEY}" ${process.env.UTILS_SITE}/BWS/_utils/_sitemap.php`)
+    console.log(`curl -X POST -d api_key=${process.env.API_KEY} ${process.env.UTILS_SITE}/BWS/_utils/_sitemap.php`)
 
     try {
-      await utils.run.command(`curl -X POST -d "api_key=${process.env.API_KEY}" ${process.env.UTILS_SITE}/BWS/_utils/_sitemap.php`)
+      await utils.run.command(`curl -X POST -d api_key=${process.env.API_KEY} ${process.env.UTILS_SITE}/BWS/_utils/_sitemap.php`)
       console.log('DB updated with new sitemap')
     } catch (error) {
       console.log('Error updating DB')
